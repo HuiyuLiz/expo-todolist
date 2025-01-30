36790d56dd4507c96880b62f4e23585eb8580189
Huiyu Li <curlyuu@gmail.com>
2025-01-31 02:27:43 +0800

    feat: update layout and add Clerk dependencies for authentication
    - Updated _layout.tsx to ensure proper default export of RootLayout component.
    - Added required Clerk dependencies for user authentication.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
+import { tokenCache } from '@/utils/cache'
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+  if (!publishableKey) {
+    throw new Error('Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env')
+  }
+
+  return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Stack />
+      </ClerkLoaded>
+    </ClerkProvider>
+  )
 }
